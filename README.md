ccbf9b7cb710de985ce1be5e05909b1de3530f44
blyxyas <blyxyas@gmail.com>
2024-06-22 19:35:39 +0200

    Add xxh3 support + true comparisons

---
+++ README.md
@@ -0,0 +1,3 @@
+# String comparison experiment
+
+What's faster, hashing a string and comparing the hash, or simply comparing the string? This Rust benchmark
--- benches/meow.rs
+++ benches/meow.rs
@@ -1,27 +1,51 @@
 use fxhash;
+use twox_hash;
+
 use divan;
+use paste::paste;
 
-#[divan::bench]
-fn hashing_short() -> bool {
-    let a = fxhash::hash(include_str!("../article.txt"));
-    let b = fxhash::hash(include_str!("../article2.txt"));
+macro_rules! generate_functions {
+    ($name: expr, $hash_fn: expr) => {
+        paste! {
+            #[divan::bench]
+            fn [<$name _short>]() -> bool {
+                let a = $hash_fn(include_bytes!("../article1.txt"));
+                let b = $hash_fn(include_bytes!("../article2.txt"));
 
-    a == b
+                // False
+                a == b
+            }
+
+            #[divan::bench]
+            fn [<$name _long>]() -> bool {
+                let a = $hash_fn(include_bytes!("../article3.txt"));
+                let b = $hash_fn(include_bytes!("../article4.txt"));
+
+                // False
+                a == b
+            }
+
+            #[divan::bench]
+            fn [<$name _true>]() -> bool {
+                let a = $hash_fn(include_bytes!("../article4.txt"));
+                let b = $hash_fn(include_bytes!("../article5.txt"));
+
+                // True
+                a == b
+            }
+        }
+    };
 }
+
+generate_functions!(fxhash, fxhash::hash);
+generate_functions!(xxh3, twox_hash::xxh3::hash64);
 
 #[divan::bench]
 fn comparison_short() -> bool {
-    let a = include_str!("../article.txt");
+    let a = include_str!("../article1.txt");
     let b = include_str!("../article2.txt");
 
-    a == b
-}
-
-#[divan::bench]
-fn hashing_long() -> bool {
-    let a = fxhash::hash(include_str!("../article3.txt"));
-    let b = fxhash::hash(include_str!("../article4.txt"));
-
+    // False
     a == b
 }
 
@@ -30,6 +54,16 @@ fn comparison_long() -> bool {
     let a = include_str!("../article3.txt");
     let b = include_str!("../article4.txt");
 
+    // False
+    a == b
+}
+
+#[divan::bench]
+fn comparison_true()-> bool {
+    let a = include_str!("../article4.txt");
+    let b = include_str!("../article5.txt");
+
+    // True
     a == b
 }
 
